7a6dfcea1cd4bcaf009cd227d32a578f55b453ec
Eren Gündüz <eren_clash@hotmail.com>
2018-11-16 14:17:27 +0300

    Add files via upload

--- Lesson 8/sayidizisiralama.cpp
+++ Lesson 8/sayidizisiralama.cpp
@@ -4,17 +4,16 @@
 
 main(){
 	srand(time(NULL));
-	int dizi[10], sayi, enbuyuk = 0, sayac=0;
+	int dizi[5], enbuyuk = 0, sayac = 0;
 	
-	for(int i=0;i<10;i++){
-		sayi = 100 + rand()%800;
-		if(sayi>enbuyuk){
-			enbuyuk=sayi;
-			dizi[sayac] = sayi;
-			sayac++;
-		}
+	while(sayac < 5){
+		int sayi = 100 + rand()%800;
+			if(sayi > enbuyuk){
+				enbuyuk = sayi;
+				dizi[sayac] = sayi;
+				sayac++;
+			}
 	}
-	
 	for(int i=0;i<5;i++){
 			printf("%d\n", dizi[i]);
 	}
